6d3fc1a7bc0ad1d8ee44e907d1915411513ead25
Park Yu Han <pbk2312@naver.com>
2024-10-04 22:51:27 +0900

    필드 위치 수정

--- src/main/java/com/example/gunpo/domain/SmokingArea.java
+++ src/main/java/com/example/gunpo/domain/SmokingArea.java
@@ -18,9 +18,9 @@ public class SmokingArea {
     @Id
     private Long id;
     private String longitude;  // 경도
+    private String latitude;   // 위도
+    private String boothName;  // 흡연부스명
     private String managementAgency; // 관리기관
     private String dataDate;  // 데이터기준일자
     private String location;   // 소재지
-    private String latitude;   // 위도
-    private String boothName;  // 흡연부스명
 }
