2e2f08a9bfc92759d042268933350ffba32fc36b
Park Yu Han <pbk2312@naver.com>
2024-10-04 22:51:02 +0900

    로그 수정

--- src/main/java/com/example/gunpo/config/StartupRunner.java
+++ src/main/java/com/example/gunpo/config/StartupRunner.java
@@ -3,12 +3,14 @@ package com.example.gunpo.config;
 import com.example.gunpo.service.SmokingZoneService;
 import lombok.RequiredArgsConstructor;
 //
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
+@Log4j2
 public class StartupRunner {
     private final SmokingZoneService smokingZoneService;
 
@@ -17,7 +19,7 @@ public class StartupRunner {
         return args -> {
             // 모든 흡연 구역 데이터를 가져와 Redis에 저장
             smokingZoneService.fetchAllDataAndSaveToRedis();
-            System.out.println("모든 흡연 구역 데이터가 Redis에 저장되었습니다.");
+            log.info("모든 흡연 구역 데이터가 Redis에 저장되었습니다.");
         };
     }
 
