7f9e5ed02a67ef6a1e984d3b5c70be42c0bdfb78
Park Yu Han <pbk2312@naver.com>
2024-10-05 00:01:25 +0900

    Redis에서 흡연구역 정보 가져오기

--- src/main/java/com/example/gunpo/service/RedisService.java
+++ src/main/java/com/example/gunpo/service/RedisService.java
@@ -1,12 +1,16 @@
 package com.example.gunpo.service;
 
 import com.example.gunpo.domain.SmokingArea;
+import com.example.gunpo.dto.SmokingAreaDto;
+import com.example.gunpo.mapper.SmokingZoneMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Log4j2
@@ -19,7 +23,6 @@ public class RedisService {
     public void saveToRedis(List<SmokingArea> smokingAreas) {
         smokingAreas.forEach(smokingArea -> {
             String redisKey = REDIS_KEY_PREFIX + smokingArea.getBoothName(); // 고유 키 설정
-
             // Redis에 이미 존재하는지 확인
             if (!redisTemplate.hasKey(redisKey)) {
                 redisTemplate.opsForValue().set(redisKey, smokingArea); // Redis에 저장
@@ -28,5 +31,26 @@ public class RedisService {
                 log.info("이미 Redis에 존재하는 흡연구역: {}", smokingArea.getBoothName());
             }
         });
+    }
+    // Redis에 저장된 흡역 구연 정보 가져오기
+    public List<SmokingAreaDto> getAllSmokingZonesFromRedis() {
+        // Redis에 저장된 모든 흡연 구역 키 가져오기 (REDIS_KEY_PREFIX로 시작하는 키)
+        Set<String> keys = redisTemplate.keys(REDIS_KEY_PREFIX + "*");
+
+        List<SmokingAreaDto> smokingZoneList = new ArrayList<>();
+
+        if (keys != null) {
+            keys.forEach(key -> {
+                // 각 키에 저장된 SmokingArea 데이터 가져오기
+                SmokingArea smokingArea = (SmokingArea) redisTemplate.opsForValue().get(key);
+                if (smokingArea != null) {
+                    // SmokingArea를 SmokingZoneAreaDto로 변환
+                    SmokingAreaDto dto = SmokingZoneMapper.INSTANCE.toDto(smokingArea);
+                    smokingZoneList.add(dto); // 리스트에 추가
+                }
+            });
+        }
+
+        return smokingZoneList; // 모든 흡연 구역 리스트 반환
     }
 }
--- src/main/java/com/example/gunpo/service/SmokingAreaService.java
+++ src/main/java/com/example/gunpo/service/SmokingAreaService.java
@@ -1,6 +1,7 @@
 package com.example.gunpo.service;
 
 import com.example.gunpo.domain.SmokingArea;
+import com.example.gunpo.dto.SmokingAreaDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -9,11 +10,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +25,7 @@ import java.util.stream.Collectors;
 @Service
 @Log4j2
 @RequiredArgsConstructor
-public class SmokingZoneService {
+public class SmokingAreaService {
 
     @Value("${service.key}")
     private String serviceKey;
@@ -32,27 +34,7 @@ public class SmokingZoneService {
     private final ObjectMapper objectMapper;
     private final RedisService redisService;
 
-    public List<SmokingArea> getDataFromAPI(int page, int size) {
-        String url = buildApiUrl(page, size);
-        log.info("요청 URL: {}", url);
-
-        try {
-            ResponseEntity<String> responseEntity = restTemplate.getForEntity(new URI(url), String.class);
-            log.info("응답 상태 코드: {}", responseEntity.getStatusCode());
-
-            return handleApiResponse(responseEntity);
-        } catch (URISyntaxException e) {
-            log.error("URI 문법 오류: {}", e.getMessage());
-        } catch (Exception e) {
-            log.error("API 호출 중 오류가 발생했습니다: {}", e.getMessage());
-        }
-
-        return Collections.emptyList();
-    }
-
-    // 모든 데이터를 가져오고 Redis에 저장하는 메서드
     public void fetchAllDataAndSaveToRedis() {
-        List<SmokingArea> allSmokingAreas = new ArrayList<>();
         int page = 1;
         int size = 100; // 페이지당 데이터 수 조정
 
@@ -62,23 +44,30 @@ public class SmokingZoneService {
                 log.info("더 이상 흡연 구역 데이터가 없습니다. 데이터 수집 종료.");
                 break; // 데이터가 없으면 종료
             }
-            allSmokingAreas.addAll(smokingAreas); // 수집된 데이터에 추가
+            // Redis에 데이터 저장
+            redisService.saveToRedis(smokingAreas);
+            log.info("{} 페이지의 흡연 구역 데이터가 Redis에 저장되었습니다.", page);
             page++;
-        }
-
-        // Redis에 모든 데이터 저장
-        if (!allSmokingAreas.isEmpty()) {
-            redisService.saveToRedis(allSmokingAreas); // RedisService에서 중복 검증 및 저장 처리
-            log.info("모든 흡연 구역 데이터가 Redis에 저장되었습니다.");
-        } else {
-            log.warn("저장할 흡연 구역 데이터가 없습니다.");
         }
     }
 
-    private String buildApiUrl(int page, int size) {
-        String encodedServiceKey = serviceKey.replace("+", "%2B");
-        return String.format("https://api.odcloud.kr/api/15122343/v1/uddi:20726a37-6b6a-4b28-8370-9097991eca6a?returnType=json&serviceKey=%s&page=%d&size=%d",
-                encodedServiceKey, page, size);
+    public List<SmokingArea> getDataFromAPI(int page, int size) {
+        String url = buildApiUrl(page, size);
+        log.info("요청 URL: {}", url);
+
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(new URI(url), String.class);
+            log.info("응답 상태 코드: {}", responseEntity.getStatusCode());
+            return handleApiResponse(responseEntity);
+        } catch (URISyntaxException e) {
+            log.error("URI 문법 오류: {}", e.getMessage());
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            log.error("API 호출 중 오류 발생: {} {}", e.getStatusCode(), e.getResponseBodyAsString());
+        } catch (Exception e) {
+            log.error("API 호출 중 알 수 없는 오류가 발생했습니다: {}", e.getMessage());
+        }
+
+        return Collections.emptyList();
     }
 
     private List<SmokingArea> handleApiResponse(ResponseEntity<String> responseEntity) {
@@ -91,17 +80,20 @@ public class SmokingZoneService {
         }
     }
 
-
     private List<SmokingArea> parseResponse(String response) {
-        try {
-            log.info("원본 응답: {}", response); // 응답 본문 로그 추가
-            Map<String, Object> map = objectMapper.readValue(response, Map.class);
-            log.info("파싱된 맵: {}", map); // 파싱된 결과 로그 추가
+        if (response == null || response.isEmpty()) {
+            log.warn("응답이 null이거나 비어 있습니다.");
+            return Collections.emptyList();
+        }
 
-            // "data" 키로 흡연 구역 정보를 가져오기
+        try {
+            log.info("원본 응답: {}", response);
+            Map<String, Object> map = objectMapper.readValue(response, Map.class);
+            log.info("파싱된 맵: {}", map);
+
             List<Map<String, Object>> itemsList = (List<Map<String, Object>>) map.get("data");
             if (itemsList != null) {
-                return extractSmokingAreas(itemsList); // itemsList를 전달
+                return extractSmokingAreas(itemsList);
             } else {
                 log.warn("응답에서 흡연 구역 데이터가 없습니다.");
             }
@@ -119,6 +111,11 @@ public class SmokingZoneService {
     }
 
     private SmokingArea mapToSmokingArea(Map<String, Object> itemMap) {
+        if (itemMap == null) {
+            log.warn("itemMap이 null입니다.");
+            return null;
+        }
+
         SmokingArea smokingArea = new SmokingArea();
         smokingArea.setLongitude((String) itemMap.get("경도"));
         smokingArea.setManagementAgency((String) itemMap.get("관리기관"));
@@ -128,4 +125,16 @@ public class SmokingZoneService {
         smokingArea.setBoothName((String) itemMap.get("흡연부스명"));
         return smokingArea;
     }
+
+    private String buildApiUrl(int page, int size) {
+        String encodedServiceKey = serviceKey.replace("+", "%2B");
+        return String.format("https://api.odcloud.kr/api/15122343/v1/uddi:20726a37-6b6a-4b28-8370-9097991eca6a?returnType=json&serviceKey=%s&page=%d&size=%d",
+                encodedServiceKey, page, size);
+    }
+
+    public List<SmokingAreaDto> getAllSmokingZones(){
+        return redisService.getAllSmokingZonesFromRedis();
+    }
+
+
 }
