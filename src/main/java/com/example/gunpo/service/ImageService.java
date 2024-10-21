e33f5c2c2c289bad094420f597ba807144719eb7
Park Yu Han <pbk2312@naver.com>
2024-10-21 14:32:29 +0900

    refactor(service): 이미지 저장 로직 수정

--- src/main/java/com/example/gunpo/service/ImageService.java
+++ src/main/java/com/example/gunpo/service/ImageService.java
@@ -3,6 +3,7 @@ package com.example.gunpo.service;
 import com.example.gunpo.domain.Board;
 import com.example.gunpo.domain.BoardImage;
 import com.example.gunpo.repository.BoardImageRepository;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,8 +26,10 @@ public class ImageService {
     private final BoardImageRepository boardImageRepository;
 
 
-    // 이미지 저장 및 게시물에 추가
-    public void saveImages(List<MultipartFile> images, Board board) {
+    // 이미지 저장 및 게시물에 추가, 저장된 이미지를 반환
+    public List<BoardImage> saveImages(List<MultipartFile> images, Board board) {
+        List<BoardImage> savedImages = new ArrayList<>(); // 저장된 이미지 리스트
+
         if (images != null && !images.isEmpty()) {
             log.info("업로드된 이미지 수: {}", images.size());
 
@@ -46,10 +49,15 @@ public class ImageService {
 
                 // BoardImage를 DB에 저장
                 boardImageRepository.save(boardImage);
+
+                // 저장된 이미지를 리스트에 추가
+                savedImages.add(boardImage);
             }
         } else {
             log.info("업로드된 이미지가 없습니다.");
         }
+
+        return savedImages; // 저장된 이미지 리스트 반환
     }
 
     // 실제 파일을 저장하는 로직
@@ -74,6 +82,8 @@ public class ImageService {
             throw new RuntimeException("이미지 저장 중 오류가 발생했습니다.");
         }
     }
+
+
 
     // BoardImage 객체 생성
     private BoardImage createBoardImage(String imagePath, Board board) {
