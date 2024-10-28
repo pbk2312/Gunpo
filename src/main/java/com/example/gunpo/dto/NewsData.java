1a245071fd740ee4ec7a7f42683b4dba8348ec68
Park Yu Han <pbk2312@naver.com>
2024-10-28 23:15:19 +0900

    refactor(dto): NewsData 클래스를 레코드로 변환하여 코드 간결화 및 불변성 확보
    - 기존 NewsData 클래스를 Java 레코드로 변경하여 코드 간결화
    - 레코드 사용으로 불변성 보장, getter/setter 불필요
    - 필드 선언 및 초기화 코드 간소화로 가독성 및 유지보수성 향상

--- src/main/java/com/example/gunpo/dto/NewsData.java
+++ src/main/java/com/example/gunpo/dto/NewsData.java
@@ -1,22 +1,3 @@
 package com.example.gunpo.dto;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public  class NewsData {
-    private final String title;
-    private final String imageUrl;
-    private final String summary;
-    private final String link;
-
-    public NewsData(String title, String imageUrl, String summary, String link) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.summary = summary;
-        this.link = link;
-    }
-
-}
+public record NewsData(String title, String imageUrl, String summary, String link) {}
