19baef19850bd00e9770880c70cc8da1ccfcf76e
Park Yu Han <pbk2312@naver.com>
2024-11-13 15:49:57 +0900

    refactor(service): RedisEmailCertificationService로 변경 및 메서드 반환 타입 수정
    - 이메일 인증 관련 Redis 서비스를 `RedisService`에서 `RedisEmailCertificationService`로 변경하여 역할 명확성 강화
    - `verifyEmail` 메서드의 반환 타입을 `void`로 수정하여 인증 성공 시 메시지 반환 대신 흐름을 단순화

--- src/main/java/com/example/gunpo/service/email/EmailService.java
+++ src/main/java/com/example/gunpo/service/email/EmailService.java
@@ -2,9 +2,8 @@ package com.example.gunpo.service.email;
 
 import com.example.gunpo.dto.EmailDto;
 import com.example.gunpo.infrastructure.EmailProvider;
-import com.example.gunpo.repository.MemberRepository;
-import com.example.gunpo.service.CertificationNumberGenerator;
-import com.example.gunpo.service.RedisService;
+import com.example.gunpo.service.member.CertificationNumberGenerator;
+import com.example.gunpo.service.redis.RedisEmailCertificationService;
 import com.example.gunpo.validator.EmailValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -17,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class EmailService {
 
     private final EmailProvider emailProvider;
-    private final RedisService redisService;
+    private final RedisEmailCertificationService redisEmailCertificationService;
     private final CertificationNumberGenerator certificationNumberGenerator;
     private final EmailValidator emailValidator;
 
@@ -27,14 +26,14 @@ public class EmailService {
 
         String certificationNumber = certificationNumberGenerator.generate();
         emailProvider.sendCertificationMail(emailDto.getEmail(), certificationNumber);
-        redisService.saveEmailCertificationToRedis(emailDto.getEmail(), certificationNumber);
+        redisEmailCertificationService.saveEmailCertificationToRedis(emailDto.getEmail(), certificationNumber);
 
         return "이메일 전송 성공";
     }
 
     @Transactional
-    public String verifyEmail(String email, String certificationNumber) {
-        String storedData = redisService.getEmailCertificationFromRedis(email);
+    public void verifyEmail(String email, String certificationNumber) {
+        String storedData = redisEmailCertificationService.getEmailCertificationFromRedis(email);
 
         emailValidator.validateStoredData(storedData);
 
@@ -42,8 +41,7 @@ public class EmailService {
         emailValidator.validateCertification(parts[0], certificationNumber);
         emailValidator.validateVerificationStatus(parts[1]);
 
-        redisService.updateEmailCertificationInRedis(email, parts[0] + ":true");
-        return "인증번호 인증 성공";
+        redisEmailCertificationService.updateEmailCertificationInRedis(email, parts[0] + ":true");
     }
 
 }
