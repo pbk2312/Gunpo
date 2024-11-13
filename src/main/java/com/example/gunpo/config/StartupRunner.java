cb65737eba1a8558cc366a013efb55fcb1a85991
Park Yu Han <pbk2312@naver.com>
2024-11-13 17:50:26 +0900

    feat(congfig): 경기 지역 화폐 가맹점 데이터를 API에서 가져와 Redis에 저장하는 서비스 구현

--- src/main/java/com/example/gunpo/config/StartupRunner.java
+++ src/main/java/com/example/gunpo/config/StartupRunner.java
@@ -1,5 +1,6 @@
 package com.example.gunpo.config;
 
+import com.example.gunpo.service.GyeonggiMerchantService;
 import com.example.gunpo.service.SmokingAreaService;
 import lombok.RequiredArgsConstructor;
 //
@@ -13,12 +14,14 @@ import org.springframework.context.annotation.Configuration;
 @Log4j2
 public class StartupRunner {
     private final SmokingAreaService smokingAreaService;
+    private final GyeonggiMerchantService gyeonggiMerchantService;
 
     @Bean
     public ApplicationRunner initializeData() {
         return args -> {
             // 모든 흡연 구역 데이터를 가져와 Redis에 저장
             smokingAreaService.fetchAllDataAndSaveToRedis();
+            gyeonggiMerchantService.fetchAllDataAndSaveToRedis();
             log.info("모든 흡연 구역 데이터가 Redis에 저장되었습니다.");
         };
     }
