e034e953f5aba0e1051790d3e3c99baa5d622a1b
Park Yu Han <pbk2312@naver.com>
2024-11-13 17:51:23 +0900

    refactor(dto): 공공데이터 필드값에 맞추기 위해 @JsonProperty 적용

--- src/main/java/com/example/gunpo/dto/RegionMnyFacltStusDto.java
+++ src/main/java/com/example/gunpo/dto/RegionMnyFacltStusDto.java
@@ -1,5 +1,6 @@
 package com.example.gunpo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,19 +10,46 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegionMnyFacltStusDto {
 
+    @JsonProperty("BIZREGNO")
     private String bizRegNo; // 사업자 등록번호
+
+    @JsonProperty("CMPNM_NM")
     private String cmpnmNm; // 상호명
+
+    @JsonProperty("INDUTYPE_NM")
     private String indutypeNm; // 업종명
+
+    @JsonProperty("REFINE_LOTNO_ADDR")
     private String refineLotnoAddr; // 지번 주소
+
+    @JsonProperty("REFINE_ROADNM_ADDR")
     private String refineRoadnmAddr; // 도로명 주소
+
+    @JsonProperty("REFINE_ZIPNO")
     private String refineZipNo; // 우편번호
+
+    @JsonProperty("REFINE_WGS84_LOGT")
     private String refineWgs84Logt; // 경도
+
+    @JsonProperty("REFINE_WGS84_LAT")
     private String refineWgs84Lat; // 위도
+
+    @JsonProperty("SIGUN_NM")
     private String sigunNm; // 시군명
+
+    @JsonProperty("INDUTYPE_CD")
     private String indutypeCd; // 업종 코드
+
+    @JsonProperty("FRCS_NO")
     private String frcsNo; // 가맹점번호
+
+    @JsonProperty("LEAD_TAX_MAN_STATE")
     private String leadTaxManState; // 사업자 상태
+
+    @JsonProperty("CLSBIZ_DAY")
     private String clsBizDay; // 폐업일
+
+    @JsonProperty("LEAD_TAX_MAN_STATE_CD")
     private String leadTaxManStateCd; // 사업자 상태 코드
 
 }
