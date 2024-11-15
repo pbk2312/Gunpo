24800cdef899a662d3fdef1a1c21e5b817404e67
Park Yu Han <pbk2312@naver.com>
2024-11-15 15:50:03 +0900

    refactor(dto): @Data 가 아닌 @Setter,@Getter 사용하도록 변경

--- src/main/java/com/example/gunpo/dto/GyeonggiCurrencyStoreDto.java
+++ src/main/java/com/example/gunpo/dto/GyeonggiCurrencyStoreDto.java
@@ -2,10 +2,12 @@ package com.example.gunpo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GyeonggiCurrencyStoreDto {
