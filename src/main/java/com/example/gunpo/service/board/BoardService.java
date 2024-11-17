8fce57a80ab0cbb461319eb2c344dc2e50710940
Park Yu Han <pbk2312@naver.com>
2024-11-17 16:33:17 +0900

    chore(service) : 개행 추가 및 불 필요한 주석 제거

--- src/main/java/com/example/gunpo/service/board/BoardService.java
+++ src/main/java/com/example/gunpo/service/board/BoardService.java
@@ -10,10 +10,8 @@ public interface BoardService {
 
     Page<BoardDto> getPosts(Pageable pageable);
 
-    // 게시물 조회 메서드
-    BoardDto getPost(Long postId, String accessToken); // 게시물 조회
+    BoardDto getPost(Long postId, String accessToken);
 
-    // 게시물 삭제 메서드 (추가적인 예시)
-    void deletePost(Long postId, String accessToken); // 삭제할 게시물 ID
+    void deletePost(Long postId, String accessToken);
 
 }
--- src/main/java/com/example/gunpo/service/board/BoardServiceImpl.java
+++ src/main/java/com/example/gunpo/service/board/BoardServiceImpl.java
@@ -77,4 +77,5 @@ public class BoardServiceImpl implements BoardService {
         int viewCount = redisViewCountService.getViewCountFromRedis(boardId);
         boardDto.setViewCount(viewCount);
     }
+
 }
