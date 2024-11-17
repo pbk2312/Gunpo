ebc16f4de3d93b70f868936ef0a71dc77dea5ed7
Park Yu Han <pbk2312@naver.com>
2024-11-17 23:59:46 +0900

    feat(controller,view) : 마이페이지 기능 추가
    - 회원 정보 확인 가능
    - 수정하기 버튼 추가

---
+++ src/main/java/com/example/gunpo/controller/view/MyPageViewController.java
@@ -0,0 +1,37 @@
+package com.example.gunpo.controller.view;
+
+
+import com.example.gunpo.domain.Member;
+import com.example.gunpo.dto.MemberDto;
+import com.example.gunpo.exception.member.MemberNotFoundException;
+import com.example.gunpo.mapper.MemberMapper;
+import com.example.gunpo.service.member.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/mypage")
+public class MyPageViewController {
+
+    private final AuthenticationService authenticationService;
+
+    @GetMapping("/profile")
+    public String profileView(@CookieValue(value = "accessToken", required = false) String accessToken,
+                              Model model
+    ) {
+        Member member = authenticationService.getUserDetails(accessToken);
+        MemberDto memberDto = MemberMapper.INSTANCE.toDto(member);
+
+        model.addAttribute("memberDto", memberDto);
+
+        return "mypage/profile";
+
+    }
+
+
+}
