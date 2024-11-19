3ce5fac0ade92dc829823ff39f4e7d0de18e9fb0
Park Yu Han <pbk2312@naver.com>
2024-11-19 16:11:41 +0900

    feat(exception) : 이메일 중복 예외 처리 추가

---
+++ src/main/java/com/example/gunpo/exception/member/EmailDuplicationException.java
@@ -0,0 +1,8 @@
+package com.example.gunpo.exception.member;
+
+public class EmailDuplicationException extends RuntimeException {
+    public EmailDuplicationException(String message) {
+        super(message);
+    }
+
+}
