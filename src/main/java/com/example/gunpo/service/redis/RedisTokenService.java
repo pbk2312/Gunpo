997da5fb91c62eccedd85046f8290e54e8898206
Park Yu Han <pbk2312@naver.com>
2024-11-20 13:47:17 +0900

    refactor(service): RedisTokenService에서 토큰 검색 기능 제거 및 코드 간소화

--- src/main/java/com/example/gunpo/service/redis/RedisTokenService.java
+++ src/main/java/com/example/gunpo/service/redis/RedisTokenService.java
@@ -1,11 +1,9 @@
 package com.example.gunpo.service.redis;
 
 import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,29 +13,11 @@ public class RedisTokenService {
     private final StringRedisTemplate stringRedisTemplate;
 
     public void setStringValue(String memberId, String token, Long expirationTime) {
-        Optional.ofNullable(stringRedisTemplate.opsForValue()).ifPresent(valueOps ->
-                valueOps.set(memberId, token, expirationTime, TimeUnit.MILLISECONDS)
-        );
+        stringRedisTemplate.opsForValue().set(memberId, token, expirationTime, TimeUnit.MILLISECONDS);
     }
 
     public void deleteStringValue(String memberId) {
         stringRedisTemplate.delete(memberId);
-    }
-
-    public String findMemberIdByRefreshToken(String refreshToken) {
-        Set<String> keys = Optional.ofNullable(stringRedisTemplate.keys("*")).orElse(Set.of());
-        return findMemberIdByToken(refreshToken, keys);
-    }
-
-    private String findMemberIdByToken(String refreshToken, Set<String> keys) {
-        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
-        for (String key : keys) {
-            String storedToken = stringValueOperations.get(key);
-            if (refreshToken.equals(storedToken)) {
-                return key;
-            }
-        }
-        return null;
     }
 
 }
