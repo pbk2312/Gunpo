0bdd79084a0785dd495b5bd1d46ce620459d4d30
Park Yu Han <pbk2312@naver.com>
2024-11-20 12:27:11 +0900

    feat(exception): 이미지 저장 예외 처리 클래스 추가
    - 이미지 저장 중 발생할 수 있는 오류를 처리하기 위한 ImageStorageException 클래스 추가
    - 메시지와 원인 예외를 전달할 수 있는 생성자 추가

---
+++ src/main/java/com/example/gunpo/exception/board/ImageStorageException.java
@@ -0,0 +1,12 @@
+package com.example.gunpo.exception.board;
+
+public class ImageStorageException extends RuntimeException {
+    public ImageStorageException(String message) {
+        super(message);
+    }
+
+    public ImageStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
