ea29dad66620b70e01ba9c6b9368c2703bcb78c5
Park Yu Han <pbk2312@naver.com>
2024-11-22 16:28:29 +0900

    feat(exception) : 동네 인증 실패 예외 추가

---
+++ src/main/java/com/example/gunpo/exception/location/NeighborhoodVerificationException.java
@@ -0,0 +1,8 @@
+package com.example.gunpo.exception.location;
+
+public class NeighborhoodVerificationException extends RuntimeException {
+
+    public NeighborhoodVerificationException(String message) {
+        super(message);
+    }
+}
