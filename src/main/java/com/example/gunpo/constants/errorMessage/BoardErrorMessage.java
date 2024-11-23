97898dacd8293856d96f422d53a27add61d20733
Park Yu Han <pbk2312@naver.com>
2024-11-23 22:43:58 +0900

    feat(constants) : CommentValidator 에 쓰이는 예외 메시지 추가

--- src/main/java/com/example/gunpo/constants/errorMessage/BoardErrorMessage.java
+++ src/main/java/com/example/gunpo/constants/errorMessage/BoardErrorMessage.java
@@ -2,10 +2,15 @@ package com.example.gunpo.constants.errorMessage;
 
 public enum BoardErrorMessage {
     MISSING_TITLE_AND_CONTENT("게시물 제목 및 내용은 필수입니다."),
-    UNAUTHORIZED_MODIFY("게시물 수정 권한이 없습니다."),
+    UNAUTHORIZED_MODIFY("게시물 수정,삭제 권한이 없습니다."),
+    UNAUTHORIZED_MODIFY_COMMENT("댓글 수정,삭제 권한이 없습니다"),
     INVALID_POST_ID("유효하지 않은 게시물 ID입니다."),
     POST_NOT_FOUND("게시물을 찾을 수 없습니다."),
-    INVALID_PAGEABLE("페이지 번호와 크기는 양수여야 합니다.");
+    INVALID_PAGEABLE("페이지 번호와 크기는 양수여야 합니다."),
+    IMAGE_NOT_NULL("이미지 객체는 null일 수 없습니다."),
+    COMMENT_NOT_NULL("댓글 객체는 null일 수 없습니다."),
+    COMMENT_NOT_FOUND("댓글을 찾을 수 없습니다.");
+
 
     private final String message;
 
@@ -17,4 +22,4 @@ public enum BoardErrorMessage {
         return message;
     }
 
-    }
+}
