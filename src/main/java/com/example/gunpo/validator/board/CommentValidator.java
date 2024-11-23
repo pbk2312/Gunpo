f3429f4278bab539f3dce61b707900f245ed5eea
Park Yu Han <pbk2312@naver.com>
2024-11-23 22:40:59 +0900

    feat(validator) : 작성자 검증 로직 추가
    - 해당 작성자가 맞는지 검증 아니면 예외

---
+++ src/main/java/com/example/gunpo/validator/board/CommentValidator.java
@@ -0,0 +1,24 @@
+package com.example.gunpo.validator.board;
+
+import com.example.gunpo.constants.errorMessage.BoardErrorMessage;
+import com.example.gunpo.domain.Comment;
+import com.example.gunpo.domain.Member;
+import com.example.gunpo.exception.member.UnauthorizedException;
+import com.example.gunpo.service.member.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommentValidator {
+
+    private final AuthenticationService authenticationService;
+
+    public void verifyAuthor(String accessToken, Comment comment) {
+        Member member = authenticationService.getUserDetails(accessToken);
+        if (!comment.getAuthor().equals(member)) {
+            throw new UnauthorizedException(BoardErrorMessage.UNAUTHORIZED_MODIFY_COMMENT.getMessage());
+        }
+    }
+
+}
