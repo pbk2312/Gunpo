2ff0327602135a363076a927832ed709bb23663b
Park Yu Han <pbk2312@naver.com>
2024-11-25 14:28:56 +0900

    chore:(app) 스케줄링 활성화를 위해 @EnableScheduling 추가
    - @Scheduled 애노테이션을 사용한 스케줄링 작업 지원을 위해 @EnableScheduling 추가
    - 애플리케이션 구동 시 스케줄링 기능 활성화

--- src/main/java/com/example/gunpo/GunpoApplication.java
+++ src/main/java/com/example/gunpo/GunpoApplication.java
@@ -2,8 +2,10 @@ package com.example.gunpo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GunpoApplication {
 
     public static void main(String[] args) {
