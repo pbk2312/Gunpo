41f3d62e557bfd472b9108b6c12ef76b562f5bf7
Park Yu Han <pbk2312@naver.com>
2024-11-27 15:01:02 +0900

    feat(repository) : DB 저장에 따른 Repository 추가

---
+++ src/main/java/com/example/gunpo/repository/SmokingAreaRepository.java
@@ -0,0 +1,9 @@
+package com.example.gunpo.repository;
+
+import com.example.gunpo.domain.SmokingArea;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SmokingAreaRepository extends JpaRepository<SmokingArea,Long> {
+
+}
