1bce62b6166eda156d97ac0b45f228ec0397e5fe
Park Yu Han <pbk2312@naver.com>
2024-11-28 22:23:18 +0900

    feat(repository) : GyeonggiCurrencyStoreRepository 추가

---
+++ src/main/java/com/example/gunpo/repository/GyeonggiCurrencyStoreRepository.java
@@ -0,0 +1,9 @@
+package com.example.gunpo.repository;
+
+import com.example.gunpo.domain.GyeonggiCurrencyStore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface GyeonggiCurrencyStoreRepository extends JpaRepository<GyeonggiCurrencyStore, Long> {
+
+}
