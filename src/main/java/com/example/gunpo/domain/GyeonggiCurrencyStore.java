c580c172bdfc85fe4e3815d68a8dcb5d7d6afdb0
Park Yu Han <pbk2312@naver.com>
2024-11-28 22:22:31 +0900

    feat(domain): GyeonggiCurrencyStore 엔티티 클래스 추가
    - GyeonggiCurrencyStore 엔티티 클래스 추가
    - 사업자 등록번호, 상호명, 업종명, 주소 등 다양한 필드 추가
    - 각 필드는 @Column 어노테이션을 사용하여 매핑
    - 기본 키로 사업자 등록번호(BIZREGNO) 설정

---
+++ src/main/java/com/example/gunpo/domain/GyeonggiCurrencyStore.java
@@ -0,0 +1,61 @@
+package com.example.gunpo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GyeonggiCurrencyStore {
+
+    @Id
+    @Column(name = "BIZREGNO") // 사업자 등록번호, 기본 키로 설정
+    private String bizRegNo;
+
+    @Column(name = "CMPNM_NM")
+    private String cmpnmNm; // 상호명
+
+    @Column(name = "INDUTYPE_NM")
+    private String indutypeNm; // 업종명
+
+    @Column(name = "REFINE_LOTNO_ADDR")
+    private String refineLotnoAddr; // 지번 주소
+
+    @Column(name = "REFINE_ROADNM_ADDR")
+    private String refineRoadnmAddr; // 도로명 주소
+
+    @Column(name = "REFINE_ZIPNO")
+    private String refineZipNo; // 우편번호
+
+    @Column(name = "REFINE_WGS84_LOGT")
+    private String refineWgs84Logt; // 경도
+
+    @Column(name = "REFINE_WGS84_LAT")
+    private String refineWgs84Lat; // 위도
+
+    @Column(name = "SIGUN_NM")
+    private String sigunNm; // 시군명
+
+    @Column(name = "INDUTYPE_CD")
+    private String indutypeCd; // 업종 코드
+
+    @Column(name = "FRCS_NO")
+    private String frcsNo; // 가맹점번호
+
+    @Column(name = "LEAD_TAX_MAN_STATE")
+    private String leadTaxManState; // 사업자 상태
+
+    @Column(name = "CLSBIZ_DAY")
+    private String clsBizDay; // 폐업일
+
+    @Column(name = "LEAD_TAX_MAN_STATE_CD")
+    private String leadTaxManStateCd; // 사업자 상태 코드
+
+}
