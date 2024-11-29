68fe2c573c574732618dce11112d810c7db1be34
Park Yu Han <pbk2312@naver.com>
2024-11-29 15:24:26 +0900

    feat(repository): 작성자로 게시글 조회를 위한 메서드 추가

--- src/main/java/com/example/gunpo/repository/BoardRepository.java
+++ src/main/java/com/example/gunpo/repository/BoardRepository.java
@@ -1,6 +1,8 @@
 package com.example.gunpo.repository;
 
 import com.example.gunpo.domain.Board;
+import com.example.gunpo.domain.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +17,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     // 게시물 ID로 좋아요 수 조회
     @Query("SELECT b.likeCount FROM Board b WHERE b.id = :boardId")
     Optional<Integer> findLikeCountByBoardId(@Param("boardId") Long boardId);
+
+    List<Board> getBoardByAuthor(Member member);
+
 
 }
