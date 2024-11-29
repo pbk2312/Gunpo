899176ccaa372d28ab1c9f7b6816a1ab9351bc7a
Park Yu Han <pbk2312@naver.com>
2024-11-29 14:16:14 +0900

    feat(repository) : 회원으로 문의 내역 조회 추가
    - findByMember

--- src/main/java/com/example/gunpo/repository/InquiryRepository.java
+++ src/main/java/com/example/gunpo/repository/InquiryRepository.java
@@ -1,7 +1,12 @@
 package com.example.gunpo.repository;
 
 import com.example.gunpo.domain.Inquiry;
+import com.example.gunpo.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InquiryRepository extends JpaRepository<Inquiry,Long> {
+
+    List<Inquiry> findByMember(Member member);
+
 }
