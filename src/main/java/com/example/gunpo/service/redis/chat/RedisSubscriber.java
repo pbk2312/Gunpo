80ec93d9b5fa2ec2e5156133d72f5a43dbe6b33e
Park Yu Han <pbk2312@naver.com>
2024-11-30 22:45:46 +0900

    chore(service) : 로그 수정

--- src/main/java/com/example/gunpo/service/redis/chat/RedisSubscriber.java
+++ src/main/java/com/example/gunpo/service/redis/chat/RedisSubscriber.java
@@ -13,7 +13,7 @@ public class RedisSubscriber implements MessageListener {
     public void onMessage(Message message, byte[] pattern) {
         String channel = new String(pattern);
         String content = new String(message.getBody());
-        log.info("채널 " + channel + "에서 메시지를 수신했습니다: " + content);
+        log.info("채널 {}에서 메시지를 수신했습니다: {}", channel, content);
     }
 
 }
