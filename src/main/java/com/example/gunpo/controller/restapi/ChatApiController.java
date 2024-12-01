ea797fc145e538d0fe28bc519fb605cdfd8531fe
Park Yu Han <pbk2312@naver.com>
2024-12-01 13:40:47 +0900

    refactor(controller): 불필요한 import 제거 및 API 기본 경로 추가
    - `ChatApiController`에서 사용되지 않는 import 제거
    - API 경로 명확화를 위해 `@RequestMapping("/api/chat")` 추가

--- src/main/java/com/example/gunpo/controller/restapi/ChatApiController.java
+++ src/main/java/com/example/gunpo/controller/restapi/ChatApiController.java
@@ -7,22 +7,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/chat")
 public class ChatApiController {
 
     private final ChatMessageService chatMessageService;
 
+
     // 모든 채팅 메시지 불러오기
-    @GetMapping("/api/chat/messages")
+    @GetMapping("/messages")
     public List<String> getChatMessages() {
         return chatMessageService.getChatMessages();
     }
 
     // 채팅 메시지 저장
-    @PostMapping("/api/chat/messages")
+    @PostMapping("/messages")
     public void saveChatMessage(@RequestBody String message) {
         chatMessageService.saveMessage(message);
     }
