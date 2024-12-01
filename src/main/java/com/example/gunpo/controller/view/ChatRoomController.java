5cee40d4fb002177dee216720b1366fc2ee07ddc
Park Yu Han <pbk2312@naver.com>
2024-12-01 13:40:01 +0900

    feat(controller): 채팅방 목록 조회 컨트롤러 메서드 추가
     - 사용자의 참여 채팅방 목록을 조회하는 `/chat/rooms` 엔드포인트 추가
     - Redis에서 사용자의 채팅방 목록을 가져와 모델에 추가
     - 참여 중인 채팅방이 없을 경우 사용자에게 메시지를 표시
     - 채팅방 목록을 보여주는 뷰(`chatRoomList`)로 데이터 전달

--- src/main/java/com/example/gunpo/controller/view/ChatRoomController.java
+++ src/main/java/com/example/gunpo/controller/view/ChatRoomController.java
@@ -3,6 +3,7 @@ package com.example.gunpo.controller.view;
 import com.example.gunpo.domain.Member;
 import com.example.gunpo.service.member.AuthenticationService;
 import com.example.gunpo.service.redis.chat.RedisChatRoomService;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +18,7 @@ public class ChatRoomController {
     private final AuthenticationService authenticationService;
     private final RedisChatRoomService redisChatRoomService;
 
-    @GetMapping("/chat/create/{nickname}")
+    @GetMapping("/chat/{nickname}")
     public String createChatRoomAndGetView(@PathVariable("nickname") String nickname,
                                            @CookieValue(value = "accessToken", required = false) String accessToken,
                                            Model model) {
@@ -25,7 +26,7 @@ public class ChatRoomController {
         Member member = getMember(accessToken);
 
         // 채팅방 생성
-        String roomId = redisChatRoomService.createChatRoom(member.getNickname(), nickname);
+        String roomId = redisChatRoomService.getOrCreateChatRoom(member.getNickname(), nickname);
 
         model.addAttribute("roomId", roomId);
         model.addAttribute("memberNickname", member.getNickname());
@@ -33,6 +34,27 @@ public class ChatRoomController {
 
         return "chatRoom";
     }
+
+    @GetMapping("/chat/rooms")
+    public String getChatRooms(@CookieValue(value = "accessToken", required = false) String accessToken,
+                               Model model) {
+
+        Member member = getMember(accessToken);
+        // 사용자의 채팅방 목록 조회
+        Map<String, Map<String, String>> chatRoomsByUser = redisChatRoomService.getChatRoomsByUser(member);
+
+        if (chatRoomsByUser.isEmpty()) {
+            model.addAttribute("message", "참여 중인 채팅방이 없습니다.");
+            return "chatRoomList"; // 빈 채팅방 목록 페이지
+        }
+
+        // 채팅방 목록을 모델에 추가
+        model.addAttribute("chatRooms", chatRoomsByUser);
+        model.addAttribute("mynickname", member.getNickname());
+
+        return "chatRoomList"; // 채팅방 목록 페이지
+    }
+
 
     private Member getMember(String accessToken) {
         return authenticationService.getUserDetails(accessToken);
