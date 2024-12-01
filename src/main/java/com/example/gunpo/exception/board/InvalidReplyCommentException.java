601d0753c5bf2649e812860e5e37c1d8d7f4fb7a
Park Yu Han <pbk2312@naver.com>
2024-12-01 16:53:27 +0900

    feat(exception): 대댓글 유효성 검사 예외 처리 클래스 InvalidReplyCommentException 추가

---
+++ src/main/java/com/example/gunpo/exception/board/InvalidReplyCommentException.java
@@ -0,0 +1,9 @@
+package com.example.gunpo.exception.board;
+
+public class InvalidReplyCommentException  extends RuntimeException{
+
+    public InvalidReplyCommentException(String message) {
+        super(message);
+    }
+
+}
