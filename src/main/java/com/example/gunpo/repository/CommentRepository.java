b174311f96bde670f4ba923967a0d1acd9506d7d
Park Yu Han <pbk2312@naver.com>
2024-12-01 16:59:05 +0900

    chore(repository): 개행 삭제

--- src/main/java/com/example/gunpo/repository/CommentRepository.java
+++ src/main/java/com/example/gunpo/repository/CommentRepository.java
@@ -3,5 +3,6 @@ package com.example.gunpo.repository;
 import com.example.gunpo.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
 }
