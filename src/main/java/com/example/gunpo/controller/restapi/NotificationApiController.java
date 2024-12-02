def96c136803362eba914d4ac1437ee42657750f
Park Yu Han <pbk2312@naver.com>
2024-12-03 00:29:15 +0900

    feat(controller): 사용자 알림 처리를 위한 NotificationApiController 추가
    사용자의 알림 데이터를 조회하기 위한 REST API 컨트롤러(NotificationApiController)를 추가
    - `/api/notifications` 엔드포인트를 통해 사용자 알림을 가져기
    - Redis를 통해 알림 데이터를 조회하고 DTO로 매핑
    - 사용자 인증은 accessToken을 사용하여 처리

---
+++ src/main/java/com/example/gunpo/controller/restapi/NotificationApiController.java
@@ -0,0 +1,40 @@
+package com.example.gunpo.controller.restapi;
+
+import com.example.gunpo.domain.Member;
+import com.example.gunpo.dto.functions.NotificationDto;
+import com.example.gunpo.service.member.AuthenticationService;
+import com.example.gunpo.service.redis.notification.NotificationRedisService;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/notifications")
+public class NotificationApiController {
+
+    private final AuthenticationService authenticationService;
+    private final NotificationRedisService notificationRedisService;
+
+    @GetMapping
+    public ResponseEntity<List<NotificationDto>> getNotifications(
+            @CookieValue(value = "accessToken", required = false) String accessToken
+    ) {
+
+        Member member = authenticationService.getUserDetails(accessToken);
+        List<String> messages = notificationRedisService.getNotifications(member.getId().toString());
+        List<NotificationDto> notifications = messages.stream()
+                .map(msg -> new NotificationDto(member.getId().toString(), msg, LocalDateTime.now()))
+                .toList();
+        log.info(notifications);
+        return ResponseEntity.ok(notifications);
+    }
+
+}
