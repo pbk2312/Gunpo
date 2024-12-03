86dcca860d33f37fbf5ef0c40ed349f5455203b6
Park Yu Han <pbk2312@naver.com>
2024-12-03 22:24:33 +0900

    feat(dto): postId 추가
    - 뷰 에서 게시물로 이동할 수 있도록 postId 추가

--- src/main/java/com/example/gunpo/dto/functions/NotificationDto.java
+++ src/main/java/com/example/gunpo/dto/functions/NotificationDto.java
@@ -4,14 +4,17 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 public class NotificationDto {
 
     private String userId; // 알림을 받을 사용자 ID
     private String message; // 알림 내용
     private LocalDateTime timestamp; // 알림 생성 시간
+    private Long postId; // 댓글 달린 게시물 ID
 
 }
