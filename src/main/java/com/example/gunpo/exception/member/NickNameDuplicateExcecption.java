c2f70c561bf88d0e9517cd8a14857958bb6d1cb9
Park Yu Han <pbk2312@naver.com>
2024-12-04 23:16:11 +0900

    feat(exception) - 닉네임 중복 여부 추가
    - exception 추가 작성

---
+++ src/main/java/com/example/gunpo/exception/member/NickNameDuplicateExcecption.java
@@ -0,0 +1,9 @@
+package com.example.gunpo.exception.member;
+
+public class NickNameDuplicateExcecption extends RuntimeException {
+
+    public NickNameDuplicateExcecption(String message) {
+        super(message);
+    }
+
+}
