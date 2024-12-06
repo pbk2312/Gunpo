3e8fcfcde2d1e5d812c15cc11a544aeb6e7e3d8a
Park Yu Han <pbk2312@naver.com>
2024-12-06 22:26:34 +0900

    feat(exception): 지원하지 않는 OAuth 제공자에 대한 예외 처리 클래스 추가

---
+++ src/main/java/com/example/gunpo/exception/member/UnsupportedProviderException.java
@@ -0,0 +1,10 @@
+package com.example.gunpo.exception.member;
+
+
+public class UnsupportedProviderException extends RuntimeException {
+
+    public UnsupportedProviderException(String message) {
+        super(message);
+    }
+
+}
