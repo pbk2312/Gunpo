73cad156c2fe79261260b28faadf767870bf844c
Park Yu Han <pbk2312@naver.com>
2024-12-07 12:35:51 +0900

    feat(domain): CustomUserDetails 클래스 추가
    - CustomUserDetails 구현
      - UserDetails와 OAuth2User 인터페이스를 동시에 구현
      - Member 엔티티를 기반으로 사용자 정보 제공
      - getAuthorities를 통해 사용자 권한 설정
      - OAuth2User의 getAttributes 및 getName 메서드 구현
    
    Spring Security와 OAuth2 통합을 위한 사용자 인증/권한 관리 로직 추가

---
+++ src/main/java/com/example/gunpo/domain/CustomUserDetails.java
@@ -0,0 +1,85 @@
+package com.example.gunpo.domain;
+
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+public class CustomUserDetails implements UserDetails, OAuth2User {
+
+    private Member member;
+
+
+    public CustomUserDetails(Member member) {
+        this.member = member;
+    }
+
+
+    public CustomUserDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public List<GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + member.getMemberRole().name())); // member에서 memberRole을 사용
+        return authorities;
+    }
+
+    // getPassword: member에서 password를 반환
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    // getUsername: member에서 id를 사용
+    @Override
+    public String getUsername() {
+        return member.getId().toString(); // member의 id를 반환
+    }
+
+    // 계정이 만료 되었는지 (true: 만료X)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정이 잠겼는지 (true: 잠기지 않음)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 비밀번호가 만료되었는지 (true: 만료X)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정이 활성화(사용가능)인지 (true: 활성화)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // OAuth2User 관련 메서드
+    private Map<String, Object> attributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return member.getNickname(); // member의 nickname 반환
+    }
+
+}
