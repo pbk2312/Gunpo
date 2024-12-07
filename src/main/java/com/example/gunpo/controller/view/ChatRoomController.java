80ba62d5ace6d913a2df03dfd451b0e2c88d77e3
Park Yu Han <pbk2312@naver.com>
2024-12-07 15:15:08 +0900

    fix(controller) : 허가 받지 않은 사용자 로그인 화면으로 가게 설정

--- src/main/java/com/example/gunpo/controller/view/ChatRoomController.java
+++ src/main/java/com/example/gunpo/controller/view/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.example.gunpo.controller.view;
 
 import com.example.gunpo.domain.Member;
+import com.example.gunpo.exception.member.UnauthorizedException;
 import com.example.gunpo.service.member.AuthenticationService;
 import com.example.gunpo.service.redis.chat.RedisChatRoomService;
 import java.util.Map;
@@ -10,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -37,22 +39,29 @@ public class ChatRoomController {
 
     @GetMapping("/chat/rooms")
     public String getChatRooms(@CookieValue(value = "accessToken", required = false) String accessToken,
+                               RedirectAttributes redirectAttributes,
                                Model model) {
+        try {
+            Member member = getMember(accessToken);
+            // 사용자의 채팅방 목록 조회
+            Map<String, Map<String, String>> chatRoomsByUser = redisChatRoomService.getChatRoomsByUser(member);
 
-        Member member = getMember(accessToken);
-        // 사용자의 채팅방 목록 조회
-        Map<String, Map<String, String>> chatRoomsByUser = redisChatRoomService.getChatRoomsByUser(member);
+            if (chatRoomsByUser.isEmpty()) {
+                model.addAttribute("message", "참여 중인 채팅방이 없습니다.");
+                return "chatRoomList"; // 빈 채팅방 목록 페이지
+            }
 
-        if (chatRoomsByUser.isEmpty()) {
-            model.addAttribute("message", "참여 중인 채팅방이 없습니다.");
-            return "chatRoomList"; // 빈 채팅방 목록 페이지
+            // 채팅방 목록을 모델에 추가
+            model.addAttribute("chatRooms", chatRoomsByUser);
+            model.addAttribute("mynickname", member.getNickname());
+
+            return "chatRoomList"; // 채팅방 목록 페이지
+
+        } catch (UnauthorizedException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "1대1 채팅은 로그인이 필요합니다.");
+            return "redirect:/login";
         }
 
-        // 채팅방 목록을 모델에 추가
-        model.addAttribute("chatRooms", chatRoomsByUser);
-        model.addAttribute("mynickname", member.getNickname());
-
-        return "chatRoomList"; // 채팅방 목록 페이지
     }
 
 
