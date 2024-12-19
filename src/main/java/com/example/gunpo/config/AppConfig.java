aeab9f3848d9a9d152b1c445517a6c835522e1b4
Park Yu Han <pbk2312@naver.com>
2024-12-19 13:12:45 +0900

    CI/CD 테스트

--- src/main/java/com/example/gunpo/config/AppConfig.java
+++ src/main/java/com/example/gunpo/config/AppConfig.java
@@ -2,7 +2,7 @@ package com.example.gunpo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-//
+
 @Configuration
 public class AppConfig {
 
